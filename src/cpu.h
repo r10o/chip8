5b10c86e14cbb21bbc27d0d5f6125b0cf946a82c
Rohan Rao <rao.rohan.a@gmail.com>
2018-03-21 23:41:31 -0400

    Removed unnecessary function declarations in the header.

--- src/cpu.h
+++ src/cpu.h
@@ -1,5 +1,6 @@
 #ifndef CPU_H
 #define CPU_H
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,40 +24,5 @@ void init_cpu();
 void execute_opcode(uint16_t opcode);
 void emulate_cycle();
 
-/* For internal use only */
-void cls_0();
-void ret_0();
-void jp_1(uint16_t address);
-void call_2(uint16_t address);
-void se_3(uint8_t nib, uint8_t byte);
-void sne_4(uint8_t nib, uint8_t byte);
-void se_5(uint8_t nib1, uint8_t nib2);
-void ld_6(uint8_t nib, uint8_t byte);
-void add_7(uint8_t nib, uint8_t byte);
-void ld_8(uint8_t nib1, uint8_t nib2);
-void or_8(uint8_t nib1, uint8_t nib2);
-void and_8(uint8_t nib1, uint8_t nib2);
-void xor_8(uint8_t nib1, uint8_t nib2);
-void add_8(uint8_t nib1, uint8_t nib2);
-void sub_8(uint8_t nib1, uint8_t nib2);
-void shr_8(uint8_t nib);
-void subn_8(uint8_t nib1, uint8_t nib2);
-void shl_8(uint8_t nib);
-void sne_9(uint8_t nib1, uint8_t nib2);
-void ld_a(uint16_t address);
-void jp_b(uint16_t address);
-void rnd_c(uint8_t nib, uint8_t byte);
-void drw_d(uint8_t nib1, uint8_t nib2, uint8_t nib3);
-void skp_e(uint8_t nib);
-void sknp_e(uint8_t nib);
-void ld_f07(uint8_t nib);
-void ld_f0a(uint8_t nib);
-void ld_f15(uint8_t nib);
-void ld_f18(uint8_t nib);
-void add_f(uint8_t nib);
-void ld_f29(uint8_t nib);
-void ld_f33(uint8_t nib);
-void ld_f55(uint8_t nib);
-void ld_f65(uint8_t nib);
 #endif
 
